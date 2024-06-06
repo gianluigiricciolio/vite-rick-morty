4a7469aff7aa9e93fb6ddbf3e892eddde783353e
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-06-06 09:56:49 +0200

    [UPD] added img and name

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <script>
+import axios from 'axios';
+
 // import components
+export default {
+  data() {
+    return {
+      url: 'https://rickandmortyapi.com/api/character',
+      characters: [],
+    }
+  },
+
+  methods: {
+    apiCall() {
+      axios
+        .get(this.url)
+        .then((response) => {
+          this.characters = response.data.results;
+          console.log(JSON.parse(JSON.stringify(this.characters)));
+        })
+
+    }
+  },
+
+  created() {
+    this.apiCall();
+  }
+}
+
+
 
 </script>
 
 <template>
   <div class="container text-center">
-    <h1>Vite Vue Template</h1>
-    <p>template di partenza per progetti Vue</p>
+    <h1>Rick and Morty characters</h1>
+    <ul>
+      <li v-for="character in characters">
+        <img :src="character.image" alt="">
+        <h3>{{ character.name }}</h3>
+      </li>
+    </ul>
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+img {
+  max-width: 200px;
+  border-radius: 50%;
+  border: 10px solid white;
+}
+</style>
